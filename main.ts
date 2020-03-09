7bf7f969d19df2b753c38be4da68098f9a2f7bdc
emilvelidjanov <velidjanov.emil@gmail.com>
2020-03-09 22:53:50 +0100

    added ngrx/store, store-devtools, effects, router-store

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 import { app } from "electron";
 import { mainWindow, createMainWindow } from "./electron/window";
+import installExtension, { REDUX_DEVTOOLS } from 'electron-devtools-installer';
 
 const indexFile: string = "./dist/index.html";
 
 app.allowRendererProcessReuse = true;
 app.on("ready", () => {
+  installExtension(REDUX_DEVTOOLS)
+    .then((name) => console.log(`Added Extension: ${name}`))
+    .catch((err) => console.log('An error occurred: ', err));
+    
   createMainWindow(indexFile);
 });
 app.on("window-all-closed", () => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([], {}),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
